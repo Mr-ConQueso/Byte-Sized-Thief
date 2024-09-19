9bb6d536a3ed095d0631765daa512e12ab5f14ba
Zubialde <138266237+Zubliade@users.noreply.github.com>
2024-09-19 12:22:59 +0200

    EscenaMovimiento

---
+++ Assets/Scripts/Player/ControllThirdPerson.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ControllThirdPerson : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+    [SerializeField] private LayerMask Ground;
+
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray, 1000f);
+
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
+
+        foreach(RaycastHit hit in hits)
+        {
+            if(((1 << hit.collider.gameObject.layer) & Ground) != 0)
+            {
+
+                if (Input.GetKey(KeyCode.Mouse1))
+                {
+                    NavMeshPath path = new NavMeshPath();
+                    navAgent.CalculatePath(hit.point, path);
+                    navAgent.SetPath(path);
+                }
+            }
+            else
+            {
+                Debug.Log("No la misma capa");
+            }
+        }
+    }
+}
