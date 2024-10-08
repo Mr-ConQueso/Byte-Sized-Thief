d3adb3acceee3edaa458348768d23f47154e95d1
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-10-09 00:40:37 +0200

    Scene Builds

--- Assets/Scripts/Gameplay/TutorialArea.cs
+++ Assets/Scripts/Gameplay/TutorialArea.cs
@@ -4,6 +4,8 @@ public class TutorialArea : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("A thing just Hit Me!!");
+
         if (other.CompareTag("Player"))
         {
             Debug.Log("A Player just Hit Me!!");
